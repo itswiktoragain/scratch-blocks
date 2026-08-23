3a4dfaa314d54437235ec4478b59ec96c58c7623
itswiktoragain <propalnie15@gmail.com>
2026-08-23 18:29:22 +0200

    Modernize sideways event message blocks

--- blocks_horizontal/event.js
+++ blocks_horizontal/event.js
@@ -19,15 +19,13 @@
  */
 
 /**
- * @fileoverview Control blocks for Scratch (Horizontal)
- * @author ascii@media.mit.edu <Andrew Sliwinski>
+ * @fileoverview Event blocks for Scratch (Horizontal / Dry Eggs).
  */
 'use strict';
 
 goog.provide('Blockly.Blocks.event');
 
 goog.require('Blockly.Blocks');
-
 goog.require('Blockly.Colours');
 
 Blockly.Blocks['event_whenflagclicked'] = {
@@ -60,40 +58,10 @@ Blockly.Blocks['event_whenflagclicked'] = {
   }
 };
 
-Blockly.Blocks['dropdown_whenbroadcast'] = {
-  /**
-   * Block for when broadcast dropdown (used for shadow).
-   * @this Blockly.Block
-   */
-  init: function() {
-    this.appendDummyInput()
-        .appendField(new Blockly.FieldIconMenu(
-            [
-              {src: Blockly.mainWorkspace.options.pathToMedia + 'icons/event_when-broadcast-received_blue.svg',
-                value: 'blue', width: 48, height: 48, alt: 'Blue'},
-              {src: Blockly.mainWorkspace.options.pathToMedia + 'icons/event_when-broadcast-received_green.svg',
-                value: 'green', width: 48, height: 48, alt: 'Green'},
-              {src: Blockly.mainWorkspace.options.pathToMedia + 'icons/event_when-broadcast-received_coral.svg',
-                value: 'coral', width: 48, height: 48, alt: 'Coral'},
-              {src: Blockly.mainWorkspace.options.pathToMedia + 'icons/event_when-broadcast-received_magenta.svg',
-                value: 'magenta', width: 48, height: 48, alt: 'Magenta'},
-              {src: Blockly.mainWorkspace.options.pathToMedia + 'icons/event_when-broadcast-received_orange.svg',
-                value: 'orange', width: 48, height: 48, alt: 'Orange'},
-              {src: Blockly.mainWorkspace.options.pathToMedia + 'icons/event_when-broadcast-received_purple.svg',
-                value: 'purple', width: 48, height: 48, alt: 'Purple'}
-            ]), 'CHOICE');
-    this.setOutput(true);
-    this.setColour(Blockly.Colours.event.primary,
-        Blockly.Colours.event.secondary,
-        Blockly.Colours.event.tertiary,
-        Blockly.Colours.event.quaternary
-    );
-  }
-};
-
 Blockly.Blocks['event_whenbroadcastreceived'] = {
   /**
-   * Block for when broadcast received.
+   * Horizontal Dry Eggs message receiver. Messages are numeric channels rather
+   * than the normal broadcast-variable dropdown.
    * @this Blockly.Block
    */
   init: function() {
@@ -106,11 +74,14 @@ Blockly.Blocks['event_whenbroadcastreceived'] = {
           "src": Blockly.mainWorkspace.options.pathToMedia + "icons/event_when-broadcast-received_blue.svg",
           "width": 40,
           "height": 40,
-          "alt": "Broadcast received"
+          "alt": "When message received"
         },
         {
-          "type": "input_value",
-          "name": "CHOICE"
+          "type": "field_number",
+          "name": "BROADCAST_OPTION",
+          "value": 1,
+          "min": 0,
+          "precision": 1
         }
       ],
       "inputsInline": true,
@@ -124,40 +95,10 @@ Blockly.Blocks['event_whenbroadcastreceived'] = {
   }
 };
 
-Blockly.Blocks['dropdown_broadcast'] = {
-  /**
-   * Block for broadcast dropdown (used for shadow).
-   * @this Blockly.Block
-   */
-  init: function() {
-    this.appendDummyInput()
-        .appendField(new Blockly.FieldIconMenu(
-            [
-              {src: Blockly.mainWorkspace.options.pathToMedia + 'icons/event_broadcast_blue.svg',
-                value: 'blue', width: 48, height: 48, alt: 'Blue'},
-              {src: Blockly.mainWorkspace.options.pathToMedia + 'icons/event_broadcast_green.svg',
-                value: 'green', width: 48, height: 48, alt: 'Green'},
-              {src: Blockly.mainWorkspace.options.pathToMedia + 'icons/event_broadcast_coral.svg',
-                value: 'coral', width: 48, height: 48, alt: 'Coral'},
-              {src: Blockly.mainWorkspace.options.pathToMedia + 'icons/event_broadcast_magenta.svg',
-                value: 'magenta', width: 48, height: 48, alt: 'Magenta'},
-              {src: Blockly.mainWorkspace.options.pathToMedia + 'icons/event_broadcast_orange.svg',
-                value: 'orange', width: 48, height: 48, alt: 'Orange'},
-              {src: Blockly.mainWorkspace.options.pathToMedia + 'icons/event_broadcast_purple.svg',
-                value: 'purple', width: 48, height: 48, alt: 'Purple'}
-            ]), 'CHOICE');
-    this.setOutput(true);
-    this.setColour(Blockly.Colours.event.primary,
-        Blockly.Colours.event.secondary,
-        Blockly.Colours.event.tertiary,
-        Blockly.Colours.event.quaternary
-    );
-  }
-};
-
 Blockly.Blocks['event_broadcast'] = {
   /**
-   * Block to send a broadcast.
+   * Horizontal Dry Eggs message sender. The toolbox supplies a math_number
+   * shadow in BROADCAST_INPUT instead of event_broadcast_menu.
    * @this Blockly.Block
    */
   init: function() {
@@ -170,11 +111,47 @@ Blockly.Blocks['event_broadcast'] = {
           "src": Blockly.mainWorkspace.options.pathToMedia + "icons/event_broadcast_blue.svg",
           "width": 40,
           "height": 40,
-          "alt": "Broadcast"
+          "alt": "Send message"
         },
         {
           "type": "input_value",
-          "name": "CHOICE"
+          "name": "BROADCAST_INPUT",
+          "check": "Number"
+        }
+      ],
+      "inputsInline": true,
+      "previousStatement": null,
+      "nextStatement": null,
+      "category": Blockly.Categories.event,
+      "colour": Blockly.Colours.event.primary,
+      "colourSecondary": Blockly.Colours.event.secondary,
+      "colourTertiary": Blockly.Colours.event.tertiary,
+      "colourQuaternary": Blockly.Colours.event.quaternary
+    });
+  }
+};
+
+Blockly.Blocks['event_broadcastandwait'] = {
+  /**
+   * Horizontal Dry Eggs message sender which waits for receivers to finish.
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.jsonInit({
+      "id": "event_broadcastandwait",
+      "message0": "%1 wait %2",
+      "args0": [
+        {
+          "type": "field_image",
+          "src": Blockly.mainWorkspace.options.pathToMedia + "icons/event_broadcast_blue.svg",
+          "width": 40,
+          "height": 40,
+          "alt": "Send message and wait"
+        },
+        {
+          "type": "input_value",
+          "name": "BROADCAST_INPUT",
+          "check": "Number"
         }
       ],
       "inputsInline": true,
